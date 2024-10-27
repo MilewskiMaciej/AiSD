bbc257d4d550da6c3a1a8f499b21978da14b62eb
MilewskiMaciej <147150198+MilewskiMaciej@users.noreply.github.com>
2024-10-27 16:10:51 +0100

    Add files via upload

---
+++ making_list/making_list/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace making_list
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            button1 = new Button();
+            label1 = new Label();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            label2 = new Label();
+            label3 = new Label();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(125, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.DarkViolet;
+            button1.Location = new Point(145, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 29);
+            button1.TabIndex = 1;
+            button1.Text = "ADDFIRST";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = Color.FromArgb(64, 64, 64);
+            label1.Location = new Point(27, 104);
+            label1.Name = "label1";
+            label1.Size = new Size(85, 20);
+            label1.TabIndex = 2;
+            label1.Text = "Stan listy: {}";
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button2.ForeColor = Color.DarkViolet;
+            button2.Location = new Point(271, 409);
+            button2.Name = "button2";
+            button2.Size = new Size(120, 29);
+            button2.TabIndex = 3;
+            button2.Text = "ADDLAST";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.DarkViolet;
+            button3.Location = new Point(397, 409);
+            button3.Name = "button3";
+            button3.Size = new Size(120, 29);
+            button3.TabIndex = 4;
+            button3.Text = "REMOVEFIRST";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = Color.DarkViolet;
+            button4.Location = new Point(523, 409);
+            button4.Name = "button4";
+            button4.Size = new Size(120, 29);
+            button4.TabIndex = 5;
+            button4.Text = "REMOVELAST";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.ForeColor = Color.FromArgb(64, 64, 64);
+            label2.Location = new Point(27, 73);
+            label2.Name = "label2";
+            label2.Size = new Size(94, 22);
+            label2.TabIndex = 6;
+            label2.Text = "Podaj liczbę:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Calibri", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.Indigo;
+            label3.Location = new Point(200, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(398, 37);
+            label3.TabIndex = 7;
+            label3.Text = "DODAWANIE LICZB DO TABLICY";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Snow;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private Button button1;
+        private Label label1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label2;
+        private Label label3;
+    }
+}
---
+++ making_list/making_list/Form1.cs
@@ -0,0 +1,52 @@
+namespace making_list
+{
+    public partial class Form1 : Form
+    {
+        private List list;
+        public Form1()
+        {
+            InitializeComponent();
+            list = new List();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int number))
+            {
+                list.addFirst(number);
+                label1.Text = "Stan listy: {" + list.ToString() + "}";
+            }
+            else
+            {
+                MessageBox.Show("To nie jest liczba. Podaj liczbê.");
+            }
+            textBox1.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(textBox1.Text, out int number))
+            {
+                list.addLast(number);
+                label1.Text = "Stan listy: {" + list.ToString() + "}";
+            }
+            else
+            {
+                MessageBox.Show("To nie jest liczba. Podaj liczbê.");
+            }
+            textBox1.Clear();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            list.removeFirst();
+            label1.Text = "Stan listy: {" + list.ToString() + "}";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            list.removeLast();
+            label1.Text = "Stan listy: {" + list.ToString() + "}";
+        }
+    }
+}

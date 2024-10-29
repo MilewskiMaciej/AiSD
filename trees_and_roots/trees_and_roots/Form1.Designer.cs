c771726432e5ecb588e265d2edd79790bc253d86
MilewskiMaciej <147150198+MilewskiMaciej@users.noreply.github.com>
2024-10-30 00:06:54 +0100

    Add files via upload

---
+++ trees_and_roots/trees_and_roots/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace trees_and_roots
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            button4 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button1.ForeColor = Color.DarkOrchid;
+            button1.Location = new Point(208, 409);
+            button1.Name = "button1";
+            button1.Size = new Size(111, 29);
+            button1.TabIndex = 0;
+            button1.Text = "ADD";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(116, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 1;
+            // 
+            // button3
+            // 
+            button3.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button3.ForeColor = Color.DarkViolet;
+            button3.Location = new Point(325, 409);
+            button3.Name = "button3";
+            button3.Size = new Size(118, 29);
+            button3.TabIndex = 5;
+            button3.Text = "REMOVEFIRST";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // button4
+            // 
+            button4.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
+            button4.ForeColor = Color.DarkViolet;
+            button4.Location = new Point(449, 409);
+            button4.Name = "button4";
+            button4.Size = new Size(118, 29);
+            button4.TabIndex = 6;
+            button4.Text = "REMOVELAST";
+            button4.UseVisualStyleBackColor = true;
+            button4.Click += button4_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(167, 183);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 20);
+            label1.TabIndex = 7;
+            label1.Text = "     ";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BorderStyle = BorderStyle.FixedSingle;
+            label2.Location = new Point(12, 112);
+            label2.Name = "label2";
+            label2.Size = new Size(87, 22);
+            label2.TabIndex = 8;
+            label2.Text = "Stan listy: {}";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 18F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.Indigo;
+            label3.Location = new Point(250, 9);
+            label3.Name = "label3";
+            label3.Size = new Size(264, 41);
+            label3.TabIndex = 9;
+            label3.Text = "DRZEWA BINARNE";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BorderStyle = BorderStyle.FixedSingle;
+            label4.Location = new Point(12, 74);
+            label4.Name = "label4";
+            label4.Size = new Size(98, 22);
+            label4.TabIndex = 10;
+            label4.Text = "Podaj liczbę: ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BorderStyle = BorderStyle.FixedSingle;
+            label5.Location = new Point(12, 150);
+            label5.Name = "label5";
+            label5.Size = new Size(343, 22);
+            label5.TabIndex = 11;
+            label5.Text = "Tutaj potencjalnie ma się pojawić drzewo binarne:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Snow;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button4);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private TextBox textBox1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+    }
+}
